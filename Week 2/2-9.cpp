d7568027a724591b0b12f10ad3ec875676381c55
92600jason <92600jason@gmail.com>
2026-03-15 19:32:46 +0900

    2주차 과제

---
+++ Week 2/2-9.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main(void) {
+	int a, b;
+	std::cout << " 입력: ";
+	std::cin >> a >> b;
+
+	if (a > b) {
+		std::cout << "더 큰 숫자는 " << a << std::endl;
+	}
+	else if (a < b) {
+		std::cout << "더 큰 숫자는 " << b << std::endl;
+	}
+	else {
+		std::cout << "같다" << std::endl;
+	}
+	return 0;
+}
