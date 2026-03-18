88b14752fa108e869fe7ba438e60e6ffe236851c
92600jason <92600jason@gmail.com>
2026-03-18 14:00:59 +0900

    과제

---
+++ Week 2/2-5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main() {
+    
+    std::cout << "--- 정사각형 ---" << std::endl;
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            std::cout << "*";
+        }
+        std::cout << std::endl; 
+    }
+
+    std::cout << "\n\n"; 
+
+    
+    std::cout << "--- 직사각형 ---" << std::endl;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            std::cout << "*";
+        }
+        std::cout << std::endl; 
+    }
+
+    std::cout << "\n\n";
+
+
+    std::cout << "--- 삼각형 ---" << std::endl;
+    for (int i = 1; i <= 5; ++i) {
+        for (int j = 0; j < i; ++j) {
+            std::cout << "*";
+        }
+        std::cout << std::endl; 
+    }
+
+    return 0;
+}
