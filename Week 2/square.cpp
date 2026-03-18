b6e943bdef4c524c28610d748a1f212ba2ba4efa
92600jason <92600jason@gmail.com>
2026-03-18 14:36:11 +0900

    Hw

---
+++ Week 2/square.cpp
@@ -0,0 +1,44 @@
+﻿//square.cpp
+#include <iostream>
+
+using std::cin;
+using std:: cout;
+using std::endl;
+
+
+//*네모 만들기
+int main(void) {
+	cout << "1. square, 2. rectangle ";
+	int option;
+	cin  >> option;
+	if (option != 1 && option != 2) {
+		cout << "Error: Only choose 1 or 2 ";
+		return 0x34AF; // 16진법 
+	}
+	cout << "what size square? ";
+
+	int size;
+	cin >> size;
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (option == 1) {
+					cout << "*"; // 10X10 사각형
+				}
+				else if (option == 2) {
+					cout << "**"; // 20X10 직사각형
+				}
+				else {
+					cout << "Error: Only choose 1 or 2 ";
+						return 0x34AF; // 16진법 
+				}
+				
+			}
+			cout << endl;
+		}
+
+
+
+
+	return 0;
+}
---
+++ Week 2/tryanlge.cpp
@@ -0,0 +1,32 @@
+﻿//tryangle.cpp
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+//*네모 만들기
+int main(void) {
+	
+	cout << "what size tryangle? ";
+
+	int size;
+	cin >> size;
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+			}
+		for (int j = 0; j < (2 * size) - 1 - (i * 2); j++) {
+			cout << "*";
+
+		}
+		cout << endl;
+	}
+
+
+
+
+	return 0;
+}
